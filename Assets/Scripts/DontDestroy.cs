cb6577bd3790d082fd615a6d2302089f58b25894
Ruben Van Dijck <rubenvandijck@hotmail.be>
2023-02-21 18:17:49 +0100

    Added input manager, major turnaround

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    [HideInInspector]
+    public string objectID;
+
+    private void Awake()
+    {
+        objectID = name+ transform.position.ToString() + transform.eulerAngles.ToString();
+    }
+
+
+    void Start()
+    {
+        for(  int i = 0; i < Object.FindObjectsOfType<DontDestroy>().Length; i++ )
+        {
+            if( Object.FindObjectsOfType<DontDestroy>()[i] != this )
+            {
+                if( Object.FindObjectsOfType<DontDestroy>()[i].objectID == objectID )
+                    Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
