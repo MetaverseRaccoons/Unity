733bb69bd5243ba0174e5bd8525c16c3ffd4e658
Ruben Van Dijck <rubenvandijck@hotmail.be>
2023-02-28 16:35:27 +0100

    Major breakthrough.

--- Assets/Scripts/SettingButtonBehaviour.cs
+++ Assets/Scripts/SettingButtonBehaviour.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Luminosity.IO;
+using TMPro;
 
 public class SettingButtonBehaviour : MonoBehaviour
 {
     public string ButtonAction;
+
+    public void Start()
+    {
+        InputAction inputAction = InputManager.GetAction("CustomControls", ButtonAction);
+        GetComponentInChildren<TextMeshProUGUI>().text = inputAction.Bindings[0].Positive.ToString();
+        Debug.Log("Changed button text.");
+    }
 
     public void changeKey()
     {
@@ -31,46 +39,48 @@ public class SettingButtonBehaviour : MonoBehaviour
             Debug.Log("Scan complete: " + result.Key.ToString() + ".");
             InputManager.Save();
             Debug.Log("Saved.");
+            GetComponentInChildren<TextMeshProUGUI>().text = result.Key.ToString();
+            Debug.Log("Changed button text.");
             return true;
         });
     }
 
-    public void changeJoystick()
-    {
-        ScanSettings settings = new ScanSettings
-        {
-            ScanFlags = ScanFlags.JoystickButton | ScanFlags.JoystickAxis;
-            // If the player presses this key the scan will be canceled.
-            CancelScanKey = KeyCode.Escape,
-            // If the player doesn't press any key within the specified number
-            // of seconds the scan will be canceled.
-            Timeout = 10
-        };
+    // public void changeJoystick()
+    // {
+    //     ScanSettings settings = new ScanSettings
+    //     {
+    //         ScanFlags = ScanFlags.JoystickButton | ScanFlags.JoystickAxis,
+    //         // If the player presses this key the scan will be canceled.
+    //         CancelScanKey = KeyCode.Escape,
+    //         // If the player doesn't press any key within the specified number
+    //         // of seconds the scan will be canceled.
+    //         Timeout = 10
+    //     };
 
-        // You can replace the lambda function with a member function if you want.
-        InputManager.StartInputScan(settings, result =>
-        {
-            // The handle should return "true" if the button is accepted or "false" if the button is rejected.
-            // If the button is rejected the scan will continue until a button is accepted or until the timeout expires.
+    //     // You can replace the lambda function with a member function if you want.
+    //     InputManager.StartInputScan(settings, result =>
+    //     {
+    //         // The handle should return "true" if the button is accepted or "false" if the button is rejected.
+    //         // If the button is rejected the scan will continue until a button is accepted or until the timeout expires.
 
-            if(result.ScanFlags == ScanFlags.JoystickButton)
-            {
-                if((int)key < (int)KeyCode.JoystickButton0 || (int)key > (int)KeyCode.JoystickButton19)
-                    return false;
+    //         if(result.ScanFlags == ScanFlags.JoystickButton)
+    //         {
+    //             if((int)result.Key < (int)KeyCode.JoystickButton0 || (int)result.Key > (int)KeyCode.JoystickButton19)
+    //                 return false;
 
-                InputAction inputAction = InputManager.GetAction("MyControlScheme", "MyAction");
-                inputAction.Bindings[0].Type = InputType.Button;
-                inputAction.Bindings[0].Positive = result.key;
-            }
-            else
-            {
-                InputAction inputAction = InputManager.GetAction("MyControlScheme", "MyAction");
-                inputAction.Bindings[0].Type = InputType.AnalogButton;
-                inputAction.Bindings[0].Invert = result.JoystickAxisValue < 0.0f;
-                inputAction.Bindings[0].Axis = result.JoystickAxis;
-            }
+    //             InputAction inputAction = InputManager.GetAction("MyControlScheme", "MyAction");
+    //             inputAction.Bindings[0].Type = InputType.Button;
+    //             inputAction.Bindings[0].Positive = result.Key;
+    //         }
+    //         else
+    //         {
+    //             InputAction inputAction = InputManager.GetAction("MyControlScheme", "MyAction");
+    //             inputAction.Bindings[0].Type = InputType.AnalogButton;
+    //             inputAction.Bindings[0].Invert = result.JoystickAxisValue < 0.0f;
+    //             inputAction.Bindings[0].Axis = result.JoystickAxis;
+    //         }
 
-            return true;
-        });
-    }
+    //         return true;
+    //     });
+    // }
 }
