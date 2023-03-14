a49785b39ab16773068cf737886c32be4123e181
Ruben Van Dijck <rubenvandijck@hotmail.be>
2023-03-14 17:30:08 +0100

    v1.0.1

--- Assets/OSK/Assets/Scripts/KeyboardDragHandler.cs
+++ Assets/OSK/Assets/Scripts/KeyboardDragHandler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-<<<<<<< HEAD
 public class KeyboardDragHandler : MonoBehaviour, IDragHandler
 {
 
@@ -22,24 +21,4 @@ public class KeyboardDragHandler : MonoBehaviour, IDragHandler
 
 
 
-=======
-public class KeyboardDragHandler : MonoBehaviour, IDragHandler
-{
-
-
-
-    #region IDragHandler implementation
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = eventData.position;
-    }
-
-    #endregion
-
-
-
-
-
->>>>>>> 412baaf9dd887cf70f5fc9e95d429f39a5b0a210
 }
--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,23 +16,3 @@ public class LoginController : MonoBehaviour
         
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LoginController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
->>>>>>> 412baaf9dd887cf70f5fc9e95d429f39a5b0a210
